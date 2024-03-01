e164c06ed1bef41de23779b521b6634da6a8ce7f
KRA-official <KRA_official@outlook.jp>
2024-03-02 03:20:21 +0900

    三体問題! version2

--- import math.py
+++ import math.py
@@ -16,32 +16,48 @@ class star() :
         self.speed_x = speed_x
         self.speed_y = speed_y
 
-A = star(-10000000,0,1 * 10^24,0,1)
-B = star(10000000,0,10 * 10^24,0,-1)
+A = star(-10000000,0,1 * 10^24,0,100000)
+B = star(10000000,0,10 * 10^24,0,-100000)
+C = star(5000000,5000000,5 * 10^24,100000,500000)
 
-distance = math.sqrt((A.x-B.x)**2 + (A.y-B.y)**2)
+distanceA_B = math.sqrt((A.x-B.x)**2 + (A.y-B.y)**2)
+distanceB_C = math.sqrt((B.x-C.x)**2 + (B.y-C.y)**2)
+distanceC_A = math.sqrt((C.x-A.x)**2 + (C.y-A.y)**2)
 
 def move() :
     AtoB_x = B.x - A.x
     AtoB_y = B.y - A.y
+    AtoC_x = C.x - A.x
+    AtoC_y = C.y - A.y
     BtoA_x = A.x - B.x
     BtoA_y = A.y - B.y
-    A.speed_x += AtoB_x * (B.math) / (2 * (distance ** 2)) * tick
-    A.speed_y += AtoB_y * (B.math) / (2 * (distance ** 2)) * tick
-    B.speed_x += BtoA_x * (A.math) / (2 * (distance ** 2)) * tick
-    B.speed_y += BtoA_y * (A.math) / (2 * (distance ** 2)) * tick
+    BtoC_x = C.x - B.x
+    BtoC_y = C.y - B.y
+    CtoA_x = A.x - C.x
+    CtoA_y = A.y - C.y
+    CtoB_x = B.x - C.x
+    CtoB_y = B.y - C.y
+    A.speed_x += (AtoB_x * (B.math) / (2 * (distanceA_B ** 2)) + AtoC_x * (C.math) / (2 * (distanceC_A ** 2))) * tick
+    A.speed_y += (AtoB_y * (B.math) / (2 * (distanceA_B ** 2)) + AtoC_y * (C.math) / (2 * (distanceC_A ** 2))) * tick
+    B.speed_x += (BtoA_x * (A.math) / (2 * (distanceA_B ** 2)) + BtoC_x * (C.math) / (2 * (distanceB_C ** 2))) * tick
+    B.speed_y += (BtoA_y * (A.math) / (2 * (distanceA_B ** 2)) + BtoC_y * (C.math) / (2 * (distanceB_C ** 2))) * tick
+    C.speed_x += (CtoB_x * (B.math) / (2 * (distanceB_C ** 2)) + CtoA_x * (A.math) / (2 * (distanceC_A ** 2))) * tick
+    C.speed_y += (CtoB_y * (B.math) / (2 * (distanceB_C ** 2)) + CtoA_y * (A.math) / (2 * (distanceC_A ** 2))) * tick
     A.x += A.speed_x
     A.y += A.speed_y
     B.x += B.speed_x
     B.y += B.speed_y
+    C.x += C.speed_x
+    C.y += C.speed_y
 
 
 k = 0
 while k < roop :
-    history.append([[A.x,A.y],[B.x,B.y]])
-    plt.plot((A.x,B.x),(A.y,B.y),".",c="black")
+    history.append([[A.x,A.y],[B.x,B.y],[C.x,C.y]])
+    plt.plot(A.x,A.y,".",c="red")
+    plt.plot(B.x,B.y,".",c="green")
+    plt.plot(C.x,C.y,".",c="blue")
     plt.savefig(f"./images/{k}.png")
-    plt.cla
     pic_name = f"{k}.png"
     img = Image.open(f"./images/{pic_name}")
     pictures.append(img)
@@ -49,12 +65,7 @@ while k < roop :
     print(k)
     k += 1
 
-plt.show()
-
-print(A.x,A.y)
-print(B.x,B.y)
-print(history)
-print(distance)
-
 pictures[0].save('output.gif',save_all=True, append_images=pictures[1:],
 optimize=True, duration=1, loop=0)
+
+print("finished!")
